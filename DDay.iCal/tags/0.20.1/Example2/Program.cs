b7fae3a1d816600c0846ac1fec1978bac09c3176
dougday <dougday@1ca100b6-b014-4248-a4f8-5dbb9f49130a>
2007-03-02 01:42:19 +0000

    made a copy

---
+++ DDay.iCal/tags/0.20.1/Example2/Program.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DDay.iCal;
+using DDay.iCal.Components;
+using DDay.iCal.DataTypes;
+using DDay.iCal.Serialization;
+
+namespace Example2
+{
+    public class Program
+    {
+        /// <summary>
+        /// Creates a string representation of an event.
+        /// </summary>
+        /// <param name="evt">The event to display</param>
+        /// <returns>A string representation of the event.</returns>
+        static string GetDescription(Event evt)
+        {
+            string summary = evt.Summary + ": " + evt.Start.Local.ToShortDateString();
+
+            if (evt.IsAllDay)
+            {
+                return summary + " (all day)";
+            }
+            else
+            {
+                summary += ", " + evt.Start.Local.ToShortTimeString();
+                return summary + " (" + Math.Round((double)(evt.End - evt.Start).TotalHours) + " hours)";
+            }
+        }
+
+        /// <summary>
+        /// The main program execution.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Create a new iCalendar
+            iCalendar iCal = new iCalendar();
+            
+            // Create the event, and add it to the iCalendar
+            Event evt = Event.Create(iCal);
+
+            // Set information about the event
+            evt.Start = DateTime.Today;
+            evt.Start = evt.Start.AddHours(8);
+            evt.End = evt.Start.AddHours(18); // This also sets the duration            
+            evt.DTStamp = DateTime.Now;
+            evt.Description = "The event description";
+            evt.Location = "Event location";
+            evt.Summary = "18 hour event summary";
+
+            // Set information about the second event
+            evt = Event.Create(iCal);
+            evt.Start = DateTime.Today.AddDays(5);            
+            evt.End = evt.Start.AddDays(1);
+            evt.IsAllDay = true;
+            evt.DTStamp = DateTime.Now;
+            evt.Summary = "All-day event";
+
+            // Display each event
+            foreach(Event e in iCal.Events)
+                Console.WriteLine("Event created: " + GetDescription(e));
+            
+            // Serialize (save) the iCalendar
+            iCalendarSerializer serializer = new iCalendarSerializer(iCal);
+            serializer.Serialize(@"iCalendar.ics");
+            Console.WriteLine("iCalendar file saved." + Environment.NewLine);
+            
+            iCal = iCalendar.LoadFromFile(@"iCalendar.ics");
+            Console.WriteLine("iCalendar file loaded.");
+            foreach (Event e in iCal.Events)
+                Console.WriteLine("Event loaded: " + GetDescription(e));
+        }
+    }
+}
