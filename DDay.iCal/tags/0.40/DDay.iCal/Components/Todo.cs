489624da24c778128dcf45755e72de0f4a2a0dcc
dougday <dougday@1ca100b6-b014-4248-a4f8-5dbb9f49130a>
2007-08-13 15:09:24 +0000

    made a copy

---
+++ DDay.iCal/tags/0.40/DDay.iCal/Components/Todo.cs
@@ -0,0 +1,280 @@
+using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Configuration;
+using DDay.iCal.Components;
+using DDay.iCal.DataTypes;
+using DDay.iCal.Serialization;
+
+namespace DDay.iCal.Components
+{
+    /// <summary>
+    /// A class that represents an RFC 2445 VTODO component.
+    /// </summary> 
+    [DebuggerDisplay("{Summary} - {Status}")]
+    public class Todo : RecurringComponent
+    {
+        #region Private Fields
+
+        private Date_Time m_Completed;
+        private Date_Time m_Due;
+        private Duration m_Duration;
+        private Geo m_Geo;
+        private bool m_Loaded = false;
+        private Text m_Location;
+        private Integer m_PercentComplete;
+        private TextCollection[] m_Resources;
+        private TodoStatus m_Status;
+
+        #endregion
+
+        #region Public Properties
+
+        [Serialized, DefaultValueType("DATE-TIME")]
+        public Date_Time Completed
+        {
+            get { return m_Completed; }
+            set { m_Completed = value; }
+        }
+
+        /// <summary>
+        /// The start date/time of the todo item.
+        /// </summary>
+        [Serialized, DefaultValueType("DATE-TIME")]
+        public override Date_Time DTStart
+        {
+            get
+            {
+                return base.DTStart;
+            }
+            set
+            {
+                base.DTStart = value;
+                ExtrapolateTimes();
+            }
+        }
+
+        /// <summary>
+        /// The due date of the todo item.
+        /// </summary>
+        [Serialized, DefaultValueType("DATE-TIME")]
+        virtual public Date_Time Due
+        {
+            get { return m_Due; }
+            set
+            {
+                m_Due = value;
+                ExtrapolateTimes();
+            }
+        }
+
+        /// <summary>
+        /// The duration of the todo item.
+        /// </summary>
+        [Serialized, DefaultValue("P")]
+        virtual public Duration Duration
+        {
+            get { return m_Duration; }
+            set
+            {
+                m_Duration = value;
+                ExtrapolateTimes();
+            }
+        }
+
+        [Serialized]
+        public Geo Geo
+        {
+            get { return m_Geo; }
+            set { m_Geo = value; }
+        }
+
+        [Serialized]
+        public Text Location
+        {
+            get { return m_Location; }
+            set { m_Location = value; }
+        }
+
+        [Serialized]
+        public Integer PercentComplete
+        {
+            get { return m_PercentComplete; }
+            set { m_PercentComplete = value; }
+        }
+
+        [Serialized]
+        public TextCollection[] Resources
+        {
+            get { return m_Resources; }
+            set { m_Resources = value; }
+        }
+
+        /// <summary>
+        /// The status of the todo item.
+        /// </summary>
+        [Serialized, DefaultValue("NEEDS_ACTION\r\n")]
+        virtual public TodoStatus Status
+        {
+            get { return m_Status; }
+            set
+            {
+                if (m_Status != value)
+                {
+                    // Automatically set/unset the Completed time, once the
+                    // component is fully loaded (When deserializing, it shouldn't
+                    // automatically track the completed time just because the
+                    // status was changed).
+                    if (m_Loaded)
+                    {
+                        if (value == TodoStatus.COMPLETED)
+                            Completed = DateTime.Now;
+                        else Completed = null;
+                    }
+
+                    m_Status = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Todo(iCalObject parent)
+            : base(parent)
+        {
+            this.Name = ComponentBase.TODO;
+        }
+
+        #endregion
+
+        #region Static Public Methods
+
+        static public Todo Create(iCalendar iCal)
+        {
+            Todo t = iCal.Create<Todo>();
+            return t;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Use this method to determine if a todo item has been completed.
+        /// This takes into account recurrence items and the previous date
+        /// of completion, if any.
+        /// </summary>
+        /// <param name="DateTime">The date and time to test.</param>
+        /// <returns>True if the todo item has been completed</returns>
+        public bool IsCompleted(Date_Time currDt)
+        {
+            if (Status == TodoStatus.COMPLETED)
+            {
+                if (Completed == null ||
+                    Completed > currDt)
+                    return true;
+
+                foreach (Period p in Periods)
+                {
+                    if (p.StartTime > Completed && // The item has recurred after it was completed
+                        currDt >= p.StartTime)     // and the current date is after or on the recurrence date.
+                        return false;
+                }                
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns 'True' if the todo item is Active as of <paramref name="currDt"/>.
+        /// An item is Active if it requires action of some sort.
+        /// </summary>
+        /// <param name="currDt">The date and time to test.</param>
+        /// <returns>True if the item is Active as of <paramref name="currDt"/>, False otherwise.</returns>
+        public bool IsActive(Date_Time currDt)
+        {
+            if (DTStart == null)
+                return !IsCompleted(currDt) && !IsCancelled();
+            else if (currDt >= DTStart)
+                return !IsCompleted(currDt) && !IsCancelled();
+            else return false;
+        }
+
+        /// <summary>
+        /// Returns True if the todo item was cancelled.
+        /// </summary>
+        /// <returns>True if the todo was cancelled, False otherwise.</returns>
+        public bool IsCancelled()
+        {
+            return Status == TodoStatus.CANCELLED;
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override List<Period> Evaluate(Date_Time FromDate, Date_Time ToDate)
+        {
+            // Add the todo itself, before recurrence rules are evaluated
+            if (DTStart != null)
+                Periods.Add(new Period(DTStart));
+
+            return base.Evaluate(FromDate, ToDate);
+        }
+
+        /// <summary>
+        /// Automatically derives property values based on others it
+        /// contains, to provide a more "complete" object.
+        /// </summary>
+        /// <param name="e"></param>        
+        public override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            m_Loaded = true;            
+        }
+
+        /// <summary>
+        /// "Flattens" a single todo recurrence into a copy of the
+        /// todo.  This essentially "extracts" a recurrence into
+        /// a fully-fledged non-recurring todo (a single instance).
+        /// </summary>
+        /// <param name="obj">The iCalObject that will contain the flattened todo</param>
+        /// <param name="p">The period (instance) to be flattened</param>
+        /// <returns>A todo which represents a single flattened todo instance</returns>
+        protected override RecurringComponent FlattenInstance(iCalObject obj, Period p)
+        {
+            Todo todo = (Todo)base.FlattenInstance(obj, p);            
+            todo.Duration = p.Duration;
+            return todo;
+        }
+
+        /// <summary>
+        /// Returns a typed copy of the Todo object.
+        /// </summary>
+        /// <returns>A typed copy of the Todo object.</returns>
+        public new Todo Copy()
+        {
+            return (Todo)base.Copy();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ExtrapolateTimes()
+        {
+            if (Due == null && DTStart != null && Duration != null)
+                Due = DTStart + Duration;
+            else if (Duration == null && DTStart != null && Due != null)
+                Duration = Due - DTStart;
+            else if (DTStart == null && Duration != null && Due != null)
+                DTStart = Due - Duration;            
+        }
+
+        #endregion
+    }
+}
---
+++ DDay.iCal/tags/0.40/DDay.iCal/DataTypes/RDate.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using DDay.iCal.Components;
+
+namespace DDay.iCal.DataTypes
+{
+    /// <summary>
+    /// An iCalendar list of recurring dates (or date exclusions)
+    /// </summary>
+    public class RDate : iCalDataType
+    {
+        #region Private Fields
+
+        private List<Period> m_Periods = new List<Period>();
+        private TZID m_TZID;
+
+        #endregion
+
+        #region Public Properties
+
+        public TZID TZID
+        {
+            get
+            {
+                if (m_TZID == null && Parameters.ContainsKey("TZID"))
+                    m_TZID = new TZID(((Parameter)Parameters["TZID"]).Values[0]);
+                return m_TZID;
+            }
+            set { m_TZID = value; }
+        }
+
+        public List<Period> Periods
+        {
+            get { return m_Periods; }
+            set { m_Periods = value; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public RDate() { }
+        public RDate(string value) : this()
+        {
+            CopyFrom((RDate)Parse(value));
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RDate)
+            {
+                RDate r = (RDate)obj;
+
+                if (!Periods.Count.Equals(r.Periods.Count))
+                    return false;
+
+                for (int i = 0; i < Periods.Count; i++)
+                    if (!Periods[i].Equals(r.Periods[i]))
+                        return false;
+
+                return true;
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 0;
+            foreach (Period p in Periods)
+                hashCode ^= p.GetHashCode();
+            return hashCode;
+        }
+ 
+        public override void CopyFrom(object obj)
+        {
+            if (obj is RDate)
+            {
+                RDate rdt = (RDate)obj;
+                foreach (Period p in rdt.Periods)
+                    Periods.Add(p.Copy());
+            }
+            base.CopyFrom(obj);
+        }
+
+        public override bool TryParse(string value, ref object obj)
+        {
+            string[] values = value.Split(',');
+            foreach (string v in values)
+            {
+                object dt = new Date_Time();
+                object p = new Period();
+                
+                //
+                // Set the iCalendar for each Date_Time object here,
+                // so that any time zones applied to these objects will be
+                // handled correctly.
+                // NOTE: fixes RRULE30 eval, where EXDATE references a 
+                // DATE-TIME without a time zone; therefore, the time zone
+                // is implied from DTSTART, and would fail to do a proper
+                // time zone lookup, because it wasn't assigned an iCalendar
+                // object.
+                //
+                if (((Date_Time)dt).TryParse(v, ref dt))
+                {
+                    ((Date_Time)dt).iCalendar = iCalendar;
+                    ((Date_Time)dt).TZID = TZID;
+                    Periods.Add(new Period((Date_Time)dt));
+                }
+                else if (((Period)p).TryParse(v, ref p))
+                {
+                    ((Period)p).StartTime.iCalendar = ((Period)p).EndTime.iCalendar = iCalendar;
+                    ((Period)p).StartTime.TZID = ((Period)p).EndTime.TZID = TZID;
+                    Periods.Add((Period)p);
+                }
+                else return false;
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public List<Period> Evaluate(Date_Time StartDate, Date_Time FromDate, Date_Time EndDate)
+        {
+            List<Period> periods = new List<Period>();
+
+            if (StartDate > FromDate)
+                FromDate = StartDate;
+
+            if (EndDate < FromDate ||
+                FromDate > EndDate)
+                return periods;
+
+            // FIXME: should we be checking which dates are returned here?
+            // Perhaps we should only return dates between FromDate and EndDate?
+            foreach (Period p in Periods)
+                if (!periods.Contains(p))
+                    periods.Add(p);
+
+            return periods;
+        }
+
+        #endregion
+    }
+}
