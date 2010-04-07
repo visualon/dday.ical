8ab545c75e3eda18e5ce772167b65b7811f11dcc
dougday <dougday@users.sf.net>
2010-04-07 16:47:49 +0000

    Changing the "New" directory to the "trunk"

---
+++ DDay.iCal/Interfaces/General/ICalendarParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDay.iCal
+{
+    public interface ICalendarParameter :
+        ICalendarObject,
+        IKeyedObject<string>
+    {
+        event EventHandler<ValueChangedEventArgs> ValueChanged;
+
+        string Value { get; set; }
+        string[] Values { get; set; }
+    }
+}
