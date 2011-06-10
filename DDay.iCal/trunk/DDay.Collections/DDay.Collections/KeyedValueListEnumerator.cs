2f480f56324d402d421a79cfc0aabd89c01f0c69
dougday <dougday@1ca100b6-b014-4248-a4f8-5dbb9f49130a>
2011-06-10 04:28:22 +0000

    Finished first working version of DDay.Collections.

---
+++ DDay.iCal/trunk/DDay.Collections/DDay.Collections/KeyedValueListEnumerator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DDay.Collections
+{
+    public class KeyedValueListEnumerator<TKey, TOriginal, TOriginalValue, TNewValue> :
+        IEnumerator<TNewValue>
+        where TOriginal : class, IKeyedObject<TKey>, IValueObject<TOriginalValue>
+        where TNewValue : TOriginalValue
+    {
+        KeyedList<TKey, TOriginal> _List;
+        TKey _Key;
+        IEnumerator<TOriginalValue> _ValueEnumerator;
+        IEnumerator<TOriginal> _ObjectEnumerator;
+
+        public KeyedValueListEnumerator(KeyedList<TKey, TOriginal> list, TKey key)
+        {
+            _List = list;
+            _Key = key;
+        }
+
+        virtual public TNewValue Current
+        {
+            get
+            {
+                if (_ValueEnumerator != null &&
+                    _ValueEnumerator.Current is TNewValue)
+                    return (TNewValue)_ValueEnumerator.Current;
+                return default(TNewValue);
+            }
+        }
+
+        virtual public void Dispose()
+        {
+            Reset();
+        }
+
+        void DisposeValueEnumerator()
+        {
+            if (_ValueEnumerator != null)
+            {
+                _ValueEnumerator.Dispose();
+                _ValueEnumerator = null;
+            }
+        }
+
+        object System.Collections.IEnumerator.Current
+        {
+            get
+            {
+                if (_ObjectEnumerator != null)
+                    return _ObjectEnumerator.Current;
+                return default(TOriginal);
+            }
+        }
+
+        private bool MoveNextList()
+        {
+            if (_ObjectEnumerator == null)
+            {
+                _ObjectEnumerator = _List.AllOf(_Key).GetEnumerator();
+            }
+
+            if (_ObjectEnumerator != null)
+            {
+                if (_ObjectEnumerator.MoveNext())
+                {
+                    DisposeValueEnumerator();
+                    if (_ObjectEnumerator.Current != null &&
+                        _ObjectEnumerator.Current.Values != null)
+                    {
+                        if (_ObjectEnumerator.Current.Values != null)
+                        {
+                            return MoveNextList();
+                        }
+                        else
+                        {
+                            _ValueEnumerator = _ObjectEnumerator.Current.Values.GetEnumerator();
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        virtual public bool MoveNext()
+        {
+            if (_ObjectEnumerator != null)
+            {
+                if (_ObjectEnumerator.MoveNext())
+                {
+                    if (_ValueEnumerator.Current is TNewValue)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return MoveNext();
+                    }
+                }
+                else
+                {
+                    DisposeValueEnumerator();
+                    if (MoveNextList())
+                        return MoveNext();
+                }
+            }
+            else
+            {
+                if (MoveNextList())
+                    return MoveNext();
+            }
+            return false;
+        }
+
+        virtual public void Reset()
+        {
+            DisposeValueEnumerator();
+            if (_ObjectEnumerator != null)
+            {
+                _ObjectEnumerator.Dispose();
+                _ObjectEnumerator = null;
+            }
+        }
+    }
+}
--- DDay.iCal/trunk/DDay.Collections/DDay.Collections/KeyedValueListProxy.cs
+++ DDay.iCal/trunk/DDay.Collections/DDay.Collections/KeyedValueListProxy.cs
@@ -14,95 +14,130 @@ namespace DDay.Collections
     [Serializable]
 #endif
     public class KeyedValueListProxy<TKey, TOriginal, TOriginalValue, TNewValue> :
-        IList<TNewValue>
-        where TOriginal : class, IKeyedObject<TKey>, IValueObject<TOriginalValue>
+        ICollection<TNewValue>
+        where TOriginal : class, IKeyedObject<TKey>, IValueObject<TOriginalValue>, new()
         where TNewValue : TOriginalValue
     {
         #region Private Fields
 
-        IKeyedValueList<TKey, TOriginal, TOriginalValue> _RealObject;
+        KeyedValueList<TKey, TOriginal, TOriginalValue> _RealObject;
+        TKey _Key;
+        TOriginal _Container;
 
         #endregion
 
         #region Constructors
 
-        public KeyedValueListProxy(IKeyedValueList<TKey, TOriginal, TOriginalValue> realObject)
+        public KeyedValueListProxy(KeyedValueList<TKey, TOriginal, TOriginalValue> realObject, TKey key)
         {
             _RealObject = realObject;
+            _Key = key;
         }
 
         #endregion
 
-        virtual public int IndexOf(TNewValue item)
-        {
-            throw new NotImplementedException();
-        }
+        #region Private Methods
 
-        virtual public void Insert(int index, TNewValue item)
+        TOriginal EnsureContainer()
         {
-            throw new NotImplementedException();
-        }
-
-        virtual public void RemoveAt(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        virtual public TNewValue this[int index]
-        {
-            get
+            if (_Container == null)
             {
-                throw new NotImplementedException();
+                // Find an item that matches our key
+                _Container = _RealObject.AllOf(_Key).FirstOrDefault();
+
+                // If no item is found, create a new object and add it to the list
+                if (_Container == default(TOriginal))
+                {
+                    _Container = new TOriginal();
+                    _Container.Key = _Key;
+                    _RealObject.Add(_Container);
+                }
             }
-            set
-            {
-                throw new NotImplementedException();
-            }
+            return _Container;
         }
+
+        #endregion
 
         virtual public void Add(TNewValue item)
         {
-            throw new NotImplementedException();
+            // Add the value to the object
+            EnsureContainer().AddValue(item);
         }
 
         virtual public void Clear()
         {
-            throw new NotImplementedException();
+            foreach (TOriginal original in _RealObject.AllOf(_Key))
+            {
+                if (original.Values != null)
+                {
+                    // Clear all values from each matching object
+                    original.SetValue(default(TOriginalValue));
+                }
+            }
         }
 
         virtual public bool Contains(TNewValue item)
         {
-            throw new NotImplementedException();
+            foreach (TOriginal original in _RealObject.AllOf(_Key))
+            {
+                if (original.ContainsValue(item))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         virtual public void CopyTo(TNewValue[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            int index = arrayIndex;
+            foreach (TOriginal original in _RealObject.AllOf(_Key))
+            {
+                if (original.Values != null)
+                {
+                    var valueArray = original.Values.ToArray();
+                    valueArray.CopyTo(array, arrayIndex + index);
+                    index += valueArray.Length;
+                }
+            }
         }
-
+        
         virtual public int Count
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                return _RealObject
+                    .AllOf(_Key)
+                    .Sum(o => o.ValueCount);
+            }
         }
 
         virtual public bool IsReadOnly
         {
-            get { throw new NotImplementedException(); }
+            get { return false; }
         }
 
         virtual public bool Remove(TNewValue item)
         {
-            throw new NotImplementedException();
+            foreach (TOriginal original in _RealObject.AllOf(_Key))
+            {
+                if (original.ContainsValue(item))
+                {
+                    original.RemoveValue(item);
+                    return true;
+                }
+            }
+            return false;
         }
 
         virtual public IEnumerator<TNewValue> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new KeyedValueListEnumerator<TKey, TOriginal, TOriginalValue, TNewValue>(_RealObject, _Key);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return new KeyedValueListEnumerator<TKey, TOriginal, TOriginalValue, TNewValue>(_RealObject, _Key);
         }
     }
 }
