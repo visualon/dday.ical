d1e8534add840586f675b103118bf27b914d11a9
Michael Kriese <michael.kriese@visualon.de>
2015-05-04 11:28:56 +0200

    Removed svn specific stuff.

--- DDay.iCal/Properties/AssemblyInfo.cs
+++ DDay.iCal/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DDay.iCal")]
@@ -15,8 +15,8 @@ using System.Security;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 [assembly: AllowPartiallyTrustedCallers]
@@ -27,9 +27,9 @@ using System.Security;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.2.$WCREV$")]
-[assembly: AssemblyFileVersion("1.0.2.$WCREV$")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
